346795bcbb827640db2107fb5fa8c31ec89c992a
Katty Barroso <51223655+kattylucy@users.noreply.github.com>
2019-08-21 13:14:10 -0700

    Add files via upload

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,77 @@
+// BANNER ALERT TOP OF THE PAGE 
+
 const alert = document.getElementById("alert-banner-close");
 
 alert.addEventListener("click", (e) => {
     let banner = document.querySelector(".alert-banner");
     banner.style.display = 'none';
 });
+
+
+
+/// event listener for the send button in the user FORM 
+
+let sendBttn = document.getElementById('send');
+let sendBanner = document.getElementById('message-sent-alert');
+let errorBanner = document.getElementById('message-error-alert')
+
+sendBttn.addEventListener('click', () => {
+
+    let userField = document.getElementById('userField').value.length;
+    let messageField = document.getElementById('messageField').value.length;
+    
+
+    if (userField === 0){
+        errorBanner.style.display = "block";
+    }
+
+    else if (messageField === 0) {
+        errorBanner.style.display = "block";
+        
+    } 
+
+    else{
+        errorBanner.style.display = "none";
+        sendBanner.style.display ="block";
+    }
+});
+
+sendBanner.addEventListener('click', () =>{
+    sendBanner.style.display = "none";
+});
+
+
+
+
+// NOTIFICATION BELL 
+
+let bellIcon = document.querySelector('.bell-icon');
+let notificationDiv = document.getElementById("notification-from-bell-icon");
+let divToClose = document.querySelectorAll('#notification-from-bell-icon div');
+let toCloseEvent = document.querySelectorAll('#notification-from-bell-icon div h3');
+
+
+document.addEventListener('click', (e) =>{
+
+    if ( e.target.classList.contains('bell-icon')){
+        notificationDiv.style.display = "block";
+        bellIcon.className = "checked";
+    }
+
+    else if(e.target.classList.contains('checked')){
+        notificationDiv.style.display = "none";
+        bellIcon.className ="bell-icon";
+    }
+
+});
+
+
+
+
+
+
+
+/// ALL 3 CHARTS 
 
 
 let callChart = document.getElementById('myChart').getContext('2d');
@@ -16,9 +84,9 @@ let chart = new Chart(callChart, {
         labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
         datasets: [{
             label: 'Traffic Data',
-            backgroundColor: 'rgb(33, 89, 141)',
-            borderColor: 'white',
-            data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500]
+            backgroundColor: 'rgba(154, 172, 190, 0.2)',
+            borderColor: 'rgba(154, 172, 190, 1)',
+            data: [500, 900, 700, 500, 600, 1200, 1100, 450, 1500, 1000, 2500]
         }]
     },
 
@@ -38,7 +106,7 @@ let chart2  = new Chart(chart2call, {
             label: 'Traffic',
             backgroundColor: 'rgb(33, 89, 141)',
             borderColor: 'white',
-            data: [250, 200, 150, 100, 50, 30, 10]
+            data: [250, 150, 80, 100, 40, 200, 10]
         }]
     },
 
@@ -54,12 +122,13 @@ let chart3  = new Chart(donut, {
         datasets: [{
             backgroundColor: [
             'rgb(33, 89, 141)',
-            'rgb(33, 89, 141, .3)',
-            'rgb(33, 89, 141), .5'
+            'rgb(33, 89, 141, .5)',
+            'rgb(33, 89, 141, .2)'
         ],
             borderColor: 'white',
-            data: [250, 200, 150]
+            data: [450, 200, 150]
         }]
     },
 
 });
+
